2632dda99ed25e3b30ba7969253cb00b7af8141f
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-22 00:56:26 +0100

    Update index.ts

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -14,3 +14,15 @@ export interface PostProps {
   title: string;
   body: string;
 }
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+}
+export interface UserProps {
+  id: number;
+  name: string;
+  email: string;
+  address: Address;
+}
