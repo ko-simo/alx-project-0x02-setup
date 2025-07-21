b2e9320bff22c54fbec5450bdd500a6da644fb6f
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-22 00:44:28 +0100

    Create Card.tsx

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CardProps } from '../../interfaces';
+
+const Card: React.FC<CardProps> = ({ title, content }) => {
+  return (
+    <div className="border rounded-md shadow-md p-6 m-4 bg-white max-w-sm mx-auto">
+      <h3 className="text-xl font-semibold mb-2">{title}</h3>
+      <p className="text-gray-700">{content}</p>
+    </div>
+  );
+};
+
+export default Card;
